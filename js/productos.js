d230639c77f6ea6b8448fcd98fc614b38bae3391
DESKTOP-MHPMC2B\User <andvila@hotmail.com>
2022-06-20 16:33:21 -0300

    Primer commit

---
+++ js/productos.js
@@ -0,0 +1,15 @@
+//const amigurumis = ["Aurora", "Jazmín", "Simba"]
+
+const precioLetra = 450
+
+const productos = []
+
+const carrito = []
+
+class Producto {
+    constructor(nombre, tipo, precio) {
+        this.nombre = nombre
+        this.tipo = tipo
+        this.precio = precio
+    }
+}
